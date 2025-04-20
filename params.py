1c95f8b4520670f55b73a5b1356db8a3a67d1179
Nikita Vasilev <nikitavasilev@Nikitas-MacBook-Pro.local>
2025-04-20 17:51:22 +0300

    my final almost commit. God help me, please

--- params.py
+++ params.py
@@ -1,9 +1,9 @@
 vocab_size = 50000
 embedding_sizes = [100, 500, 1000]
 L = 5  # количество предыдущих слов для предсказания следующего
-epochs=10
-epochs_word2vec = 10
+epochs=25
+epochs_word2vec = 3
 learning_rate=0.01
-batch_size_word2vec=32
-batch_size=32
+batch_size_word2vec=1024
+batch_size=512
 verbose=1
